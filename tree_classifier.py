fe95499ca60da092aaadbae290596363912a7990
Yurii Zinchuk <yurii.zinchuk@ucu.edu.ua>
2022-02-20 19:22:18 +0200

    a little structure, added function to calculate mean accuracy for several datasets

--- tree_classifier.py
+++ tree_classifier.py
@@ -1,17 +1,18 @@
 import numpy as np
-from sklearn.datasets import load_iris
+from sklearn import datasets
 from sklearn.model_selection import train_test_split
+from tqdm import tqdm
 
 
 class Node:
-    def __init__(self, X, y, gini, left=None, right=None, value=None):
+    def __init__(self, X, y, gini, value=None):
         self.X = X
         self.y = y
         self.gini = gini
         self.feature_index = 0
         self.threshold = 0
-        self.left = left
-        self.right = right
+        self.left = None
+        self.right = None
         self.value = value
 
 
@@ -20,13 +21,14 @@ class MyDecisionTreeClassifier:
         self.map_depth = max_depth
 
     def fit(self, X, y):
-        self.tree = self.build_tree(X, y)
+        self.root = self.build_tree(X, y)
 
     def build_tree(self, X, y, depth=0):
         node = Node(
             X, y, self.gini(X, y),
             value=np.argmax([np.sum(y == i) for i in range(len(set(y)))])
         )
+
         if depth < self.map_depth:
             feature_index, threshold_value = self.split_data(X, y)
 
@@ -34,10 +36,12 @@ class MyDecisionTreeClassifier:
                 left_indexes = X[:, feature_index] <= threshold_value
                 X_left, y_left = X[left_indexes], y[left_indexes]
                 X_right, y_right = X[~left_indexes], y[~left_indexes]
+
                 node.feature_index = feature_index
                 node.threshold = threshold_value
                 node.left = self.build_tree(X_left, y_left, depth+1)
                 node.right = self.build_tree(X_right, y_right, depth+1)
+
         return node
 
     def split_data(self, X, y):
@@ -46,14 +50,18 @@ class MyDecisionTreeClassifier:
 
         gini_best = self.gini(X, y)
         feature_best, threshold_best = None, None
+
         num_features = X.shape[1]
-        num_samples = X.shape[0]
         for feature_index in range(num_features):
-            thresholds = X[:, feature_index]
+            values = X[:, feature_index]
+            thresholds = [(values[i-1]+values[i]) /
+                          2 for i in range(1, len(values))]
+
             for threshold in thresholds:
                 left_indexes = X[:, feature_index] <= threshold
                 X_left, y_left = X[left_indexes], y[left_indexes]
                 X_right, y_right = X[~left_indexes], y[~left_indexes]
+
                 gini_left = self.gini(X_left, y_left)
                 gini_right = self.gini(X_right, y_right)
                 gini_curr = (y_left.size/y.size)*gini_left + \
@@ -77,7 +85,7 @@ class MyDecisionTreeClassifier:
         return np.array(prediction)
 
     def _predict(self, sample):
-        node = self.tree
+        node = self.root
         while node.left:
             if sample[node.feature_index] <= node.threshold:
                 node = node.left
@@ -86,19 +94,28 @@ class MyDecisionTreeClassifier:
         return node.value
 
 
+def find_mean_accuracy(num_of_iter: int = 100, base: str = 'wine'):
+    if base == 'wine':
+        base = datasets.load_wine()
+    elif base == 'breast_cancer':
+        base = datasets.load_breast_cancer()
+    elif base == 'iris':
+        base = datasets.load_iris()
+
+    total = 0
+    for _ in tqdm(range(num_of_iter)):
+        X, y = base.data, base.target
+        X, X_test, y, y_test = train_test_split(X, y, test_size=0.20)
+
+        my_dtc.fit(X, y)
+
+        predictions = my_dtc.predict(X_test)
+        total += sum(predictions == y_test) / len(y_test)
+
+    return total/num_of_iter
+
+
 if __name__ == "__main__":
-    iris = load_iris()
-    X = iris.data[:, :]
-    y = iris.target
+    my_dtc = MyDecisionTreeClassifier(4)
 
-    X, X_test, y, y_test = train_test_split(X, y, test_size=0.20)
-
-    my_dtc = MyDecisionTreeClassifier(2)
-    my_dtc.fit(X, y)
-
-    predictions = my_dtc.predict(X_test)
-
-    print('predictions ', predictions)
-    print('test target ', y_test)
-
-    print(sum(predictions == y_test) / len(y_test))
+    print(find_mean_accuracy(100))
